cbfc5f0ec4495177db3c02987597794bdba486ef
Linda Zhang <zlindacz@gmail.com>
2016-06-28 10:43:48 -0700

    tags

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").map { |e| e.strip.downcase }.uniq
+    new_or_found_tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
