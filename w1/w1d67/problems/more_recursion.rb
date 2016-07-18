525f2a5014042ed8d313f87c0d9127c48a5038d2
Linda Zhang <zlindacz@gmail.com>
2016-07-17 22:42:06 -0700

    finished recursion

--- w1/w1d67/problems/more_recursion.rb
+++ w1/w1d67/problems/more_recursion.rb
@@ -2,40 +2,50 @@
 #sum of the integers.
 
 def sum_recur(array)
-
+  return 0 if array.empty?
+  array.first + sum_recur(array.drop(1))
 end
 
 #Problem 2: You have array of integers. Write a recursive solution to determine
 #whether or not the array contains a specific value.
 
 def includes?(array, target)
-
+  return false if array.length == 0
+  return true if array.first == target
+  includes?(array.drop(1), target)
 end
 
 # Problem 3: You have an unsorted array of integers. Write a recursive solution
 # to count the number of occurrences of a specific value.
 
 def num_occur(array, target)
-
+  return 0 if array.empty?
+  count = array.first == target ? 1 : 0
+  count + num_occur(array.drop(1), target)
 end
 
 # Problem 4: You have array of integers. Write a recursive solution to determine
 # whether or not two adjacent elements of the array add to 12.
 
 def add_to_twelve?(array)
-
+  return false if array.size <= 1
+  return true if array.first + array[1] == 12
+  add_to_twelve?(array.drop(1))
 end
 
 # Problem 5: You have array of integers. Write a recursive solution to determine
 # if the array is sorted.
 
 def sorted?(array)
-
+  return true if array.size <= 1
+  return false if array.first > array[1]
+  sorted?(array.drop(1))
 end
 
 # Problem 6: Write a recursive function to reverse a string. Don't use any
 # built-in #reverse methods!
 
 def reverse(string)
-  
+  return string if string.length <= 1
+  string[-1] + reverse(string.slice(1, -2)) + string[0]
 end
